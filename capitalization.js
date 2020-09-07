3e96a6bc735ea23fbd38d04b07fe039c620acd71
MateuszWalowski <mat.walowski@gmail.com>
2020-09-07 11:52:55 +0200

    Second version

---
+++ capitalization.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function capitalize(str) {
+
+    let Capitalization = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase()
+
+    console.log(Capitalization)
+
+}
+
+
+let name = "peteR"
+
+capitalize(name)
--- name-parts.js
+++ name-parts.js
@@ -1,19 +1,24 @@
 "use strict";
 
-const name = "Peter Heronimous Lind";
 
-const firstspace = name.indexOf(" ");
+function getNameParts(fullname) {
 
-const firstName = name.slice(0, firstspace);
-console.log("first name:" + firstName);
+    const firstspace = fullname.name.indexOf(" ");
+    const firstName = fullname.name.slice(0, firstspace);
+    const firstafter = firstspace + 1;
+    const secondspace = fullname.name.indexOf(" ", firstafter);
+    const middleName = fullname.name.slice(firstafter, secondspace);
+    const lastName = fullname.name.slice(secondspace + 1);
 
-const firstafter = firstspace + 1;
+    console.log({
+        firstName,
+        middleName,
+        lastName
+    });
+}
 
-const secondspace = name.indexOf(" ", firstafter);
+const name = {
+    name: "Peter Heronimous Lind"
+}
 
-const middleName = name.slice(firstafter, secondspace);
-console.log("middle name:" + middleName);
-
-
-const lastName = name.slice(secondspace + 1)
-console.log("last  name:" + lastName);
+getNameParts(name)
